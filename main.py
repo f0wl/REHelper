6a3b6daf41de75398eaa7fa02ff4805e78c72df5
Jan Neduchal <honza.neduchal@gmail.com>
2019-09-01 19:07:32 +0200

    Added verbose switch functionality

--- main.py
+++ main.py
@@ -22,76 +22,77 @@ with open(args.inname, "rb") as f:
     else:
         pe.print_basic_info(args.inname)
 
-# variables
-interesting_strings = util.file_interesting_strings(args.inname)
-tmp = []
-y_packer = ""
-y_malware = ""
-y_antitricks = ""
-y_crypto = ""
-y_cve = ""
-# logic
-## Interesting strings
-for x in interesting_strings:
-            string = ""
-            for c, y in enumerate(interesting_strings[x]):
-                if c % 4 == 0 and c > 0:
-                    string += "\n"
-                string += y + " "
-            if not string.strip():
-                string = util.RED + "None" + util.RESET
-            tmp.append([x + ":", string])
+if args.verbose:
+    # variables
+    interesting_strings = util.file_interesting_strings(args.inname)
+    tmp = []
+    y_packer = ""
+    y_malware = ""
+    y_antitricks = ""
+    y_crypto = ""
+    y_cve = ""
+    # logic
+    ## Interesting strings
+    for x in interesting_strings:
+                string = ""
+                for c, y in enumerate(interesting_strings[x]):
+                    if c % 4 == 0 and c > 0:
+                        string += "\n"
+                    string += y + " "
+                if not string.strip():
+                    string = util.RED + "None" + util.RESET
+                tmp.append([x + ":", string])
 
-strings_table = [
-            ["Interesting Strings", "STILL IN BETA ;)"],
-        ]
+    strings_table = [
+                ["Interesting Strings", "STILL IN BETA ;)"],
+            ]
 
-for x in tmp:
-    strings_table.append(x)
-tmp = []
+    for x in tmp:
+        strings_table.append(x)
+    tmp = []
 
-## YARA
-for c, match in enumerate(util.yara_packer(args.inname)):
-    if c % 4 == 0 and c > 0:
-        y_packer += "\n"
-    y_packer += str(match) + " "
-for c, match in enumerate(util.yara_malware(args.inname)):
-    if c % 4 == 0 and c > 0:
-        y_malware += "\n"
-    y_malware += str(match) + " "
-for c, match in enumerate(util.yara_antitricks(args.inname)):
-    if c % 6 == 0 and c > 0:
-        y_antitricks += "\n"
-    y_antitricks += str(match) + " "
-for c, match in enumerate(util.yara_cve(args.inname)):
-    if c % 6 == 0 and c > 0:
-        y_cve += "\n"
-    y_cve += str(match) + " "
-for c, match in enumerate(util.yara_crypto(args.inname)):
-    if c % 6 == 0 and c > 0:
-        y_crypto += "\n"
-    y_crypto += str(match) + " "
-yara_table = [
-    ["Yara category", "Match"],
-]
-if not y_packer.strip():
-    y_packer = util.RED + "None" + util.RESET
-if not y_malware.strip():
-    y_malware = util.RED + "None" + util.RESET
-if not y_cve.strip():
-    y_cve = util.RED + "None" + util.RESET
-if not y_antitricks.strip():
-    y_antitricks = util.RED + "None" + util.RESET
-if not y_crypto.strip():
-    y_crypto = util.RED + "None" + util.RESET
-yara_table.append(["Signatures:", y_packer])
-yara_table.append(["Malware:", y_malware])
-yara_table.append(["CVE:", y_cve])
-yara_table.append(["Antitricks:", y_antitricks])
-yara_table.append(["Crypto:", y_crypto])
+    ## YARA
+    for c, match in enumerate(util.yara_packer(args.inname)):
+        if c % 4 == 0 and c > 0:
+            y_packer += "\n"
+        y_packer += str(match) + " "
+    for c, match in enumerate(util.yara_malware(args.inname)):
+        if c % 4 == 0 and c > 0:
+            y_malware += "\n"
+        y_malware += str(match) + " "
+    for c, match in enumerate(util.yara_antitricks(args.inname)):
+        if c % 6 == 0 and c > 0:
+            y_antitricks += "\n"
+        y_antitricks += str(match) + " "
+    for c, match in enumerate(util.yara_cve(args.inname)):
+        if c % 6 == 0 and c > 0:
+            y_cve += "\n"
+        y_cve += str(match) + " "
+    for c, match in enumerate(util.yara_crypto(args.inname)):
+        if c % 6 == 0 and c > 0:
+            y_crypto += "\n"
+        y_crypto += str(match) + " "
+    yara_table = [
+        ["Yara category", "Match"],
+    ]
+    if not y_packer.strip():
+        y_packer = util.RED + "None" + util.RESET
+    if not y_malware.strip():
+        y_malware = util.RED + "None" + util.RESET
+    if not y_cve.strip():
+        y_cve = util.RED + "None" + util.RESET
+    if not y_antitricks.strip():
+        y_antitricks = util.RED + "None" + util.RESET
+    if not y_crypto.strip():
+        y_crypto = util.RED + "None" + util.RESET
+    yara_table.append(["Signatures:", y_packer])
+    yara_table.append(["Malware:", y_malware])
+    yara_table.append(["CVE:", y_cve])
+    yara_table.append(["Antitricks:", y_antitricks])
+    yara_table.append(["Crypto:", y_crypto])
 
 
-# print
-print(AsciiTable(table_data=strings_table, ).table)
-print("")
-print(AsciiTable(table_data=yara_table, ).table)
+    # print
+    print(AsciiTable(table_data=strings_table, ).table)
+    print("")
+    print(AsciiTable(table_data=yara_table, ).table)
